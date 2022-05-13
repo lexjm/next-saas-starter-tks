ff217d9473a79d410b6a3ace5bc129a98f2215d6
Alexander <alexanderjm20@gmail.com>
2022-05-13 10:47:14 -0500

    Update pricing.tsx

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -5,7 +5,7 @@ import PricingTablesSection from 'views/PricingPage/PricingTablesSection';
 
 export default function PricingPage() {
   return (
-    <Page title="Pricing" description="Cupidatat et reprehenderit ullamco aute ullamco anim tempor.">
+    <Page title="Pricing" description="Pick a plan based on your needs">
       <Wrapper>
         <PricingTablesSection />
         <FaqSection />
