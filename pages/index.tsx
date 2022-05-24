da1e76e402d6d5255b4456875f5fa328dd17aeb4
lexjm <alexanderjm20@gmail.com>
2022-05-24 13:43:46 -0500

    UPDATE

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,10 +27,10 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
         <WhiteBackgroundContainer>
           <Hero />
           <Partners />
-          <BasicSection imageUrl="/demo-illustration-1.svg" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
+          <BasicSection imageUrl="/demo-illustration-1.svg" title="Lorem ipsum dolor sit amet consectetur." overTitle="Ya está todo listo.">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
-              voluptate quo deleniti animi laboriosam.{' '}
+            Sáltate la dolorosa configuración y los devops. Netlify es un entorno global y listo para la producción desde el principio. Omita 
+            toda la configuración del servidor y vaya directamente a la construcción.{' '}
               <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Lorem ipsum, dolor sit amet
               consectetur adipisicing elit. Soluta repellendus quia quos obcaecati nihil. Laudantium non accusantium, voluptate eum nesciunt
               at suscipit quis est soluta?
